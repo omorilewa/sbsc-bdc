045d9f7130b4746b4c9d46d6312a71f24921aad2
oreoluwade <oreoluwade@gmail.com>
2018-05-25 14:37:11 +0100

    convert map to FlatList in ManageUsers component

--- src/components/ManageUsers.js
+++ src/components/ManageUsers.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { UsersTableHeader, UserListItem } from '.';
 import { allUsers } from '../util';
 import { UserListStyles as styles } from '../styles';
@@ -9,29 +9,38 @@ class ManageUsers extends Component {
     title: 'MANAGE USERS',
   }
 
+  state = {
+    usersData: allUsers,
+  }
+
   render() {
+    const { usersData } = this.state;
+
     return (
       <View>
         <UsersTableHeader />
-          {allUsers.map((item, index) => (
-          <View
-            key={index}
-            style={(index % 2 === 0) ?
-              styles.usersView :
-              [styles.usersView, styles.listBGColor]}>
-              <UserListItem
-                id={item["#"]}
-                Name={item.Name}
-                Username={item.Username}
-                Role={item.Role}
-                Status={item.Status}
-                key={index}/>
+        <FlatList
+          data={usersData}
+          renderItem={({item, index}) => (
+            <View
+              key={index}
+              style={(index % 2 === 0) ?
+                styles.usersView :
+                [styles.usersView, styles.listBGColor]}>
+                <UserListItem
+                  id={item["#"]}
+                  Name={item.Name}
+                  Username={item.Username}
+                  Role={item.Role}
+                  Status={item.Status}
+                  key={index}/>
             </View>
-          ))}
+          )}
+        />
       </View>
     );
   }
-}
+};
 
 export default ManageUsers;
 
