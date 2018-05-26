04ad3c248566ea104c1b5d0cda8893af990cfa0a
oreoluwade <oreoluwade@gmail.com>
2018-05-26 21:39:52 +0100

    alter transformUsers conditional

--- src/util/sorter.js
+++ src/util/sorter.js
@@ -54,7 +54,7 @@ export const sortLocation = (data) => {
 }
 
 export const transformUsers = (rawData) => {
-  if (rawData.usersConnection !== null) {
+  if (rawData) {
     const rawDataCopy = rawData.usersConnection.edges.slice() || [];
     return rawDataCopy.reduce((acc, curr, index) => {
       const accItem = {
