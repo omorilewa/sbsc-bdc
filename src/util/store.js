64c1fcafb1faa06d7b243b7803b8d8d60bf301da
oreoluwade <oreoluwade@gmail.com>
2018-05-28 16:39:07 +0100

    remove unused files and chunks of code

--- src/util/store.js
+++ src/util/store.js
@@ -5,15 +5,12 @@ import {
 } from "redux";
 import { reducer as formReducer } from "redux-form";
 import thunk from "redux-thunk";
-import { rateReducer, loginReducer } from '../reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middlewares = [thunk];
 
 const reducer = combineReducers({
   form: formReducer,
-  rate: rateReducer,
-  user: loginReducer,
 });
 
 const store = createStore(
