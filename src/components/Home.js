59b743736c71f289ed2c0eaf1442127393291cfb
oreoluwade <oreoluwade@gmail.com>
2018-05-30 17:14:20 +0100

    refactor Home component to accept username or email for login

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,7 @@ import { Field } from "redux-form";
 import { Mutation } from 'react-apollo';
 import { HomeScreenStyles as styles } from "../styles";
 import RenderInput from "../components/RenderInput";
-import {
-  StyledText as Text,
-  AppConsumer,
-} from ".";
+import { StyledText as Text } from ".";
 import Loader from "./Loader";
 import { AUTHENTICATE_USER, removeItem } from '../util';
 
@@ -45,74 +42,69 @@ class Home extends PureComponent {
       visibleHeight,
     } = this.props
     return (
-      <AppConsumer>
-        {({ changeUsername }) => (
-          <ImageBackground
-            source={require("../assets/images/bg.jpg")}
-            style={{ width: "100%", height: "100%" }}
-          >
-            <View
-              style={{
-                height: visibleHeight
-              }}
-            >
-              <View style={styles.main}>
-                {showLogo &&
-                  <View style={styles.logoContainer}>
-                    <Image
-                      style={styles.image}
-                      source={require("../assets/images/logo.png")}
-                      />
-                  </View>
-                }
-                <Text style={this.toggleTextStyle(styles)}>BDC FX RATE ENGINE</Text>
-                {error && <Text style={styles.error}>{errorText}</Text>}
-                <Field
-                  name="username"
-                  component={RenderInput}
-                  placeholder="Username"
-                  username
-                />
-                <Field
-                  name="password"
-                  component={RenderInput}
-                  placeholder="Password"
-                  icon-name="icon_password.png"
-                  secureTextEntry
-                />
-                <Mutation mutation={AUTHENTICATE_USER} onCompleted={loginUser} onError={showError}>
-                  {(login, { data, loading, error }) => {
-                    if (loading) {
-                      return <Loader loading={loading} />;
-                    }
-                    return (
-                      <View style={styles.buttonView}>
-                        <Button
-                          block
-                          rounded
-                          style={styles.getStartedButton}
-                          onPress={handleSubmit(async (values) => {
-                            const { password } = values;
-                            let { username } = values;
-                            username = username.toLowerCase();
-                            removeItem('token');
-                            removeErrorText();
-                            changeUsername(username);
-                            await login({
-                              variables: { username, password }
-                            });
-                          })}>
-                          <Text style={styles.buttonText}>Get Started</Text>
-                        </Button>
-                      </View>
-                    )}}
-                </Mutation>
+      <ImageBackground
+        source={require("../assets/images/bg.jpg")}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <View
+          style={{
+            height: visibleHeight
+          }}
+        >
+          <View style={styles.main}>
+            {showLogo &&
+              <View style={styles.logoContainer}>
+                <Image
+                  style={styles.image}
+                  source={require("../assets/images/logo.png")}
+                  />
               </View>
-            </View>
-          </ImageBackground>
-        )}
-      </AppConsumer>
-    )
+            }
+            <Text style={this.toggleTextStyle(styles)}>BDC FX RATE ENGINE</Text>
+            {error && <Text style={styles.error}>{errorText}</Text>}
+            <Field
+              name="usernameOrEmail"
+              component={RenderInput}
+              placeholder="Username or Email"
+              username
+            />
+            <Field
+              name="password"
+              component={RenderInput}
+              placeholder="Password"
+              icon-name="icon_password.png"
+              secureTextEntry
+            />
+            <Mutation mutation={AUTHENTICATE_USER} onCompleted={loginUser} onError={showError}>
+              {(login, { data, loading, error }) => {
+                if (loading) {
+                  return <Loader loading={loading} />;
+                }
+                return (
+                  <View style={styles.buttonView}>
+                    <Button
+                      block
+                      rounded
+                      style={styles.getStartedButton}
+                      onPress={handleSubmit(async (values) => {
+                        const { password } = values;
+                        let { usernameOrEmail } = values;
+                        usernameOrEmail = usernameOrEmail.toLowerCase();
+                        removeItem('token');
+                        removeErrorText();
+                        await login({
+                          variables: { usernameOrEmail, password }
+                        });
+                      })}>
+                      <Text style={styles.buttonText}>Get Started</Text>
+                    </Button>
+                  </View>
+                )}}
+            </Mutation>
+          </View>
+        </View>
+      </ImageBackground>
+    );
   }
 }
 
