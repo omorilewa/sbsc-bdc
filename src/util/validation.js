9a29419768284dde4c69c2436a40342c182bd91c
omorilewa <mariam3105@gmail.com>
2018-06-01 22:04:41 +0100

    add minimum of six characters for password

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,7 +1,9 @@
 export const required = value => (value ? undefined : 'Required');
 export const number = value =>
-  value && isNaN(Number(value)) ? 'Must be a number' : undefined
+value && isNaN(Number(value)) ? 'Must be a number' : undefined
 export const emailValidate = (value) => {
   const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]? w+)*(\.\w{2,3})+$/;
   return !emailRegex.test(value) ? 'Invalid email' : undefined;
 };
+export const passwordValidate = value =>
+  value && value.length < 6 ? 'Password must be at least 6 characters' : undefined
